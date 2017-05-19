880ec216a6024cb8e89c909e81bbf30c498eeb5d
Denis Nazarenko <dnazarex@gmail.com>
2017-05-19 14:37:17 +0300

    Fix bug with start page

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,21 +3,26 @@ import passport from 'passport';
 
 const router = express.Router();
 
-router.get('/dashboard', (req, res) => {
-  const { user } = req;
-  if (user) {
-    res.render('profile', { user });
+router.get('/login', passport.authenticate('oauth2'));
+
+router.get('/', function (req, res, next) {
+  if (req.query.code) {
+    next();
   } else {
     res.render('index');
   }
+  }, passport.authenticate('oauth2', {
+      failureRedirect: '/'
+    }
+  ), (req, res) => {
+  const { user } = req;
+  res.render('profile', { user });
 });
 
-router.get('/login', passport.authenticate('oauth2'));
-
-router.get('/', passport.authenticate('oauth2', {
-    failureRedirect: '/index',
-    successRedirect: '/dashboard'
-  }
-));
+router.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
 export default router;
+
